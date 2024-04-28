689869f0d44a38a128145581c6fcfe971847f019
Marlon Zambrano <81921693+marloz03@users.noreply.github.com>
2024-04-28 12:50:39 -0500

    Add files via upload

--- model_deployment/proyecto1/m10_model_deployment.py
+++ model_deployment/proyecto1/m10_model_deployment.py
@@ -20,7 +20,7 @@ def predict_price(year, mileage, state, make, model, mapping, encoder) :
     dataTesting_encoded.drop(['State', 'Make', 'Model'], axis=1, inplace=True)
 
     # Make prediction
-    y_test_pred = rgr.predict(dataTesting_encoded)[0]
+    y_test_pred = round(rgr.predict(dataTesting_encoded)[0], 2)
     print(y_test_pred)
 
     return y_test_pred
