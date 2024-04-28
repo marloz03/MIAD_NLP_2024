16b549953b36760c8f83690607d5d6c24bc2586f
Marlon Zambrano <81921693+marloz03@users.noreply.github.com>
2024-04-28 12:45:11 -0500

    Add files via upload

--- model_deployment/proyecto1/m10_model_deployment.py
+++ model_deployment/proyecto1/m10_model_deployment.py
@@ -9,25 +9,19 @@ from sklearn.preprocessing import OneHotEncoder
 def predict_price(year, mileage, state, make, model, mapping, encoder) :
 
     #Cargo el modelo para predecir
-    rgr = joblib.load(os.path.dirname(__file__) + '/car_price.pkl') 
-    print(os.path.dirname(__file__) + '/car_price.pkl')
-    print(rgr)
+    rgr = joblib.load(os.path.dirname(__file__) + '/car_price.pkl')
 
     dataTesting = pd.DataFrame([[year, mileage, state, make, model]], columns=['Year', 'Mileage', 'State', 'Make', 'Model'])
-    print(dataTesting)
 
     dataTesting['Model_Encoded'] = dataTesting['Model'].map(mapping)
     encoded_features = encoder.transform(dataTesting[['State', 'Make']])
     encoded_df = pd.DataFrame(encoded_features.toarray(), columns=encoder.get_feature_names_out(['State', 'Make']))
-    print(encoded_df)
     dataTesting_encoded = pd.concat([dataTesting, encoded_df], axis=1)
     dataTesting_encoded.drop(['State', 'Make', 'Model'], axis=1, inplace=True)
 
-    print(dataTesting_encoded)
-    print(rgr.predict(dataTesting_encoded))
-
     # Make prediction
-    y_test_pred = rgr.predict(dataTesting_encoded)#[0]
+    y_test_pred = rgr.predict(dataTesting_encoded)[0]
+    print(y_test_pred)
 
     return y_test_pred
 
