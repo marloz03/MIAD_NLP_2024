4af163739719bbf2e2a5803ccd36beed2af8acae
Marlon Zambrano <81921693+marloz03@users.noreply.github.com>
2024-04-28 12:17:04 -0500

    Add files via upload

---
+++ model_deployment/proyecto1/m10_model_deployment.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import pandas as pd
+import joblib
+import sys
+import os
+
+def predict_price(year, mileage, state, make, model, mapping) :
+
+    #Cargo el modelo para predecir
+    rgr = joblib.load(os.path.dirname(__file__) + '/car_price.pkl') 
+
+    dataTesting = pd.DataFrame([[year, mileage, state, make, model]], columns=['Year', 'Mileage', 'State', 'Make', 'Model'])
+
+    dataTesting['Model_Encoded'] = dataTesting['Model'].map(mapping)
+    encoded_features = encoder.transform(dataTesting[['State', 'Make']])
+    encoded_df = pd.DataFrame(encoded_features.toarray(), columns=encoder.get_feature_names_out(['State', 'Make']))
+    dataTesting_encoded = pd.concat([dataTesting, encoded_df], axis=1)
+    dataTesting_encoded.drop(['State', 'Make', 'Model'], axis=1, inplace=True)
+
+    # Make prediction
+    y_test_pred = rgr.predict(x_test_df)[0,1]
+
+    return y_test_pred
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 1:
+        print('Por favor agregue los siguientes datos: Year, Mileage, State, Make, Model')
+        
+    else:
+        #Cargo la data de entrenamiento para el mapping
+        dataTraining = pd.read_csv('dataTrain_carListings.zip')
+        model_means = dataTraining.groupby('Model')['Price'].mean()
+        model_mapping = model_means.to_dict()
+
+        year= sys.argv[1]
+        mileage= sys.argv[2]
+        state= sys.argv[3]
+        make=sys.argv[4]
+        model=sys.argv[5]
+        mapping = model_mapping
+
+        precio_estimado = predict_price(year, mileage, state, make, model, mapping)
+        
+        print('Precio estimado: ', precio_estimado)
+        
