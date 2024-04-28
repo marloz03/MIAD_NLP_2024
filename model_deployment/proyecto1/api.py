075609632a101b014df2605cff98293f1c2374b1
Marlon Zambrano <81921693+marloz03@users.noreply.github.com>
2024-04-28 12:19:43 -0500

    Update api.py

--- model_deployment/proyecto1/api.py
+++ model_deployment/proyecto1/api.py
@@ -5,6 +5,7 @@ from flask_restx import Api, Resource, fields
 from m10_model_deployment import predict_price
 import joblib
 from flask_cors import CORS
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes and origins
